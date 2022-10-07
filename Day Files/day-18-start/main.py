a5a9a275389625889e181af7c8c4dedd0689aeb3
VeinyMushroom <evanextence13243546@gmail.com>
2022-10-06 23:53:50 -0500

    rnmd day-18-start.py > color-dot.py / + main.py

---
+++ Day Files/day-18-start/main.py
@@ -0,0 +1,18 @@
+import turtle
+
+turt1 = turtle.Turtle()
+turt2 = turtle.Turtle()
+
+screen = turtle.Screen()
+screen.screensize(canvheight=600, canvwidth=600)
+
+turt1.color("red")
+turt1.penup
+
+turt2.color("green")
+turt2.penup
+
+
+
+
+screen.exitonclick()
