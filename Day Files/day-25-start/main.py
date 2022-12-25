a97ba486a01dc599fa1999dd41faa87e3e78e24c
VeinyMushroom <evanextence13243546@gmail.com>
2022-12-24 21:32:09 -0900

    started working on next project

---
+++ Day Files/day-25-start/main.py
@@ -0,0 +1,12 @@
+# with open("weather_data.csv") as weather_data:
+#     data = weather_data.readlines()
+# print(data)
+
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = list(csv.reader(data_file))
+    temperatures = []
+    for row in data[1:]:
+        temperatures.append(int(row[1]))
+    print(temperatures)
