b3d51ff722691becc3dc79baf06890de8592c7e0
VeinyMushroom <evanextence13243546@gmail.com>
2023-01-06 09:18:15 -0600

    funky fresh commit

--- Day Files/day-25-start/main.py
+++ Day Files/day-25-start/main.py
@@ -2,11 +2,19 @@
 #     data = weather_data.readlines()
 # print(data)
 
-import csv
+# import csv
 
-with open("/Users/eleanorextence/Desktop/100 Days Files/Day Files/day-25-start/weather_data.csv") as data_file:
-    data = list(csv.reader(data_file))
-    temperatures = []
-    for row in data[1:]:
-        temperatures.append(int(row[1]))
-    print(temperatures)
+# with open("/Users/eleanorextence/Desktop/100 Days Files/Day Files/day-25-start/weather_data.csv") as data_file:
+#     data = list(csv.reader(data_file))
+#     temperatures = []
+#     for row in data[1:]:
+#         temperatures.append(int(row[1]))
+#     print(temperatures)
+
+
+import pandas
+
+data = pandas.read_csv("/Users/eleanorextence/Desktop/100 Days Files/Day Files/day-25-start/weather_data.csv")
+print(list(data["temp"]))
+print(type(data))
+
